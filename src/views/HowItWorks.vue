c59f70ef0efd95de947d683f47271c9c9136547e
Emiliano Errecalde <eerrecalde@gmail.com>
2018-11-20 23:03:43 +0000

    First commit

---
+++ src/views/HowItWorks.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>How it works:</h1>
+    <p>...</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'How It Works',
+  methods: {
+    mounted() {
+      console.log("test");
+    },
+  },
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  lintOnSave: 'error',
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+  },
+};
